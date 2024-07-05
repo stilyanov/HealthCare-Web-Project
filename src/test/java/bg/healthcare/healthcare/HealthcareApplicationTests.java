2f4157550bbe8cad0aa70672a71ef5a0e303df05
stilyanov <v.stilyanov@gmail.com>
2024-07-05 23:08:17 +0300

    Add simple templates

---
+++ src/test/java/bg/healthcare/healthcare/HealthcareApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.healthcare.healthcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
