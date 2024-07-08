db4055943f231d6da616f3e9fc4ef05075dbb2e0
stilyanov <v.stilyanov@gmail.com>
2024-07-08 19:43:54 +0300

    Updated templates

---
+++ src/main/java/bg/softuni/healthcare/controller/AppointmentController.java
@@ -0,0 +1,13 @@
+package bg.softuni.healthcare.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppointmentController {
+
+    @GetMapping("/appointment")
+    public String appointment(){
+        return "appointment";
+    }
+}
---
+++ src/main/java/bg/softuni/healthcare/service/DoctorService.java
@@ -0,0 +1,7 @@
+package bg.softuni.healthcare.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DoctorService {
+}
