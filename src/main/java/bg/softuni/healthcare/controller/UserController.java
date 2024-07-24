137ac5cb7585e7016711bf67d21b937716dd224d
stilyanov <v.stilyanov@gmail.com>
2024-07-24 15:42:33 +0300

    Refactor User, login and registration controller

--- src/main/java/bg/softuni/healthcare/controller/UserController.java
+++ src/main/java/bg/softuni/healthcare/controller/UserController.java
@@ -1,13 +1,16 @@
 package bg.softuni.healthcare.controller;
 
+import bg.softuni.healthcare.model.dto.UserLoginDTO;
 import bg.softuni.healthcare.model.dto.UserProfileDTO;
+import bg.softuni.healthcare.model.dto.UserRegisterDTO;
 import bg.softuni.healthcare.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/users")
@@ -18,10 +21,47 @@ public class UserController {
 
     @GetMapping("/profile/{userId}")
     public String getProfile(@PathVariable Long userId, Model model) {
-        UserProfileDTO userProfileDTO = this.userService.getUserProfileById(userId);
-        model.addAttribute("userProfile", userProfileDTO);
-
+        model.addAttribute("userProfile", userService.getUserProfileById(userId));
         return "user-profile";
     }
 
+    @ModelAttribute("loginDTO")
+    public UserLoginDTO loginDTO() {
+        return new UserLoginDTO();
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/login-error")
+    public String loginError(Model model) {
+        model.addAttribute("errorMessage", "Invalid email or password!");
+        return "login-error";
+    }
+
+    @ModelAttribute("registerDTO")
+    public UserRegisterDTO registerDTO() {
+        return new UserRegisterDTO();
+    }
+
+    @GetMapping("/register")
+    public String register(Model model) {
+        if (!model.containsAttribute("registerDTO")) {
+            model.addAttribute("registerDTO", new UserRegisterDTO());
+        }
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@Valid UserRegisterDTO registerDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            redirectAttributes.addFlashAttribute("registerDTO", registerDTO);
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.registerDTO", bindingResult);
+            return "redirect:/users/register";
+        }
+        userService.registerUser(registerDTO);
+        return "redirect:/users/login";
+    }
 }
